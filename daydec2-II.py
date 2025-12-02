58c41ab81480fc5ffdd5d64f81b8378f474a021d
barath <mabarathkumar@gmail.com>
2025-12-02 23:16:08 +0530

    a few fixes is needed to slove this problem

--- daydec2-II.py
+++ daydec2-II.py
@@ -3,20 +3,27 @@
 #orelse false 
 
 
-def canJump():
-    nums = [2,3,1,1,4]
-    p1 = 0
-    while p1 < len(nums) -1:
-            p1  = nums[p1]
-            if p1 > len(nums) - 1:
-                return False  
-            elif nums[p1] ==0:
-                return False
-            elif p1 == len(nums) - 1:
-                return True
-            else:
-                p1 +=1 
+#what logic i think in my mind is like 
+#keeping a pointer varible move them until 
+#reasign the variable to the pointer and check with like steps == len(nums) - 1: len(nums) is real index of the nums 
+#our focus the jumping is can reach the position towards the last index 
+nums = [2,3,1,1,4]
+p1 = 0
 
+def canJump(nums,p1):
+    steps = p1
+    while True:
+        p1 = nums[p1]
+        steps = p1
+        if steps > len(nums) - 1: 
+            return False
+        elif nums[steps] == 0:
+            return False
+        elif nums[steps] == nums[-1]:
+            return True
+        else:
+            p1 +=1
+            steps = 0
 
-result = canJump()
+result = canJump(nums,p1)
 print(result)
