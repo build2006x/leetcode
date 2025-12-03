9a2c66c1300c077ea7831f60daf4c9d8fd201e31
barath kumar <mabarathkumar@gmail.com>
2025-12-03 23:56:16 +0530

    dec 3 i still working on the jump game sum

--- daydec2-II.py
+++ daydec2-II.py
@@ -2,11 +2,11 @@
 #help us to move to the last means we have to send true 
 #orelse false 
 
-
 #what logic i think in my mind is like
 #keeping a pointer varible move them 
 #reasign the variable to the pointer and check with like steps == len(nums) - 1: len(nums) is real index of the nums 
 #our focus the jumping is can reach the position towards the last index 
+
 nums = [2,3,1,1,4]
 p1 = 0
 
@@ -14,16 +14,15 @@ def canJump(nums,p1):
     steps = p1
     while True:
         p1 = nums[p1]
-        steps = p1
+        steps = max(steps,p1)
         if steps > len(nums) - 1: 
             return False
         elif nums[steps] == 0:
             return False
-        elif nums[steps] == nums[-1]:
+        elif steps == len(nums)-1:
             return True
         else:
-            p1 +=1
-            steps = 0
+            p1 +=1 
 
 result = canJump(nums,p1)
 print(result)
