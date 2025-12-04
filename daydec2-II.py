0f58612828a47a254ae464ce64e9cf4bebadf457
barath kumar <mabarathkumar@gmail.com>
2025-12-04 18:55:49 +0530

    jump sum done change in the dec 4

--- daydec2-II.py
+++ daydec2-II.py
@@ -7,22 +7,23 @@
 #reasign the variable to the pointer and check with like steps == len(nums) - 1: len(nums) is real index of the nums 
 #our focus the jumping is can reach the position towards the last index 
 
-nums = [2,3,1,1,4]
-p1 = 0
 
-def canJump(nums,p1):
-    steps = p1
-    while True:
-        p1 = nums[p1]
-        steps = max(steps,p1)
-        if steps > len(nums) - 1: 
-            return False
-        elif nums[steps] == 0:
-            return False
-        elif steps == len(nums)-1:
-            return True
-        else:
-            p1 +=1 
 
-result = canJump(nums,p1)
+#today i worked on the solution of the jump sum still i will solve soon 
+
+def jump_game():
+    nums = [2,3,0,1,4]
+    left_step = nums[0]
+    for i in range(len(nums)):
+       left_step -=1
+       if i < 0:
+           return False
+       elif i == len(nums) - 1:
+           return True
+       left_step = max(left_step,nums[i])
+
+
+result = jump_game()
+
+
 print(result)
