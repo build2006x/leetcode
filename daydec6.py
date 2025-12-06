95afeae2a6f4489de8eecd1087bf33a96678dbb4
barath kumar <mabarathkumar@gmail.com>
2025-12-06 17:46:05 +0530

    dec 6

---
+++ daydec6.py
@@ -0,0 +1,17 @@
+#today i working on the database sum like 
+
+#finding the duplicate email like more the onces where the email is appered means 
+#i have to ensure the checking and say which email is being more than onces 
+
+#succesfully soled the sum like 
+
+# two things i learned by doing this sum like group by and having function 
+#group by is help you to group the values with condition to filter out the desired output 
+
+#question is like finding email where duplicate ones from the person table 
+
+
+#select email 
+#from person 
+#group by person 
+#having count(*) > 2
