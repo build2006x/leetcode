0d07a07f09edc58c216a3842bead3323388d1544
barath kumar <mabarathkumar@gmail.com>
2025-12-07 14:26:37 +0530

    3sum leetcode sums

---
+++ day7dec.py
@@ -0,0 +1,22 @@
+#today 3sum is the problem 
+
+nums = [-1,0,1,2,-1,-4]
+
+nums = set(nums)
+print(nums)
+
+# result = []
+# final = []
+
+# for i in range(len()-2):
+#     final.append([nums[i],nums[i+1],nums[i+2]])
+#     if nums[i] + nums[i+1] + nums[i+2]== 0:
+#         result.append([nums[i],nums[i+1],nums[i+2]])
+
+# print(final)
+# print(result)
+
+
+#first i done this problem to make it thinking to printing the three number
+
+#two method i thinking in mind like two poitner and silding window 
