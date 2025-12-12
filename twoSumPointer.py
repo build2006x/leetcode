a932295fd957794865e1bf70d3f2a2f75caca533
barath kumar <mabarathkumar@gmail.com>
2025-12-12 14:38:00 +0530

    two sum pointer is working the main learning is like sorted only working in the two pointer

--- twoSumPointer.py
+++ twoSumPointer.py
@@ -1,4 +1,4 @@
-arr  = [2,7,5,11]
+arr  = [2,5,7,11]
 
 p1 = 0
 p2 = len(arr) - 1
@@ -6,7 +6,8 @@ target = 9
 
 while p1 < p2:
      if arr[p1] + arr[p2] == target:
-          print('yes got it',[p1,p2])
+          print('true')
+          break
      elif arr[p1] + arr[p2] > target:
           p2 -=1
      else:
