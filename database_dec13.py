b9301c8ca2abbfdcd99183cfdf0cbf65248dce87
barath kumar <mabarathkumar@gmail.com>
2025-12-13 12:55:16 +0530

    database sum 2

---
+++ database_dec13.py
@@ -0,0 +1,16 @@
+#today i work on the database problem 
+#where the problem is like there is two table 
+#one is customer table another one is orders 
+
+#like i have to find the name from the customer table of name where doesnt make a order 
+#through the reference id like the key in the orders table 
+
+#query sql
+#select Customers.name as Customers
+#from Customers
+#where Customers.id
+#not in( select Orders.id not in Orders)
+#
+
+
+#likely i learned so far of from this like not in concept to find the ppl not in this 
