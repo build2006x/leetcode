31b46315e8db15c3c9e16a75b6d1e2b2ed0571b9
barath kumar <mabarathkumar@gmail.com>
2025-12-23 05:59:54 +0530

    apply operation on array sum completed

--- day15dec.py
+++ day15dec.py
@@ -1,37 +1,23 @@
-#this sum is like appling the operation on a array 
-
-##
 nums = [1,2,2,1,1,0]
 
- 
-point1 = 0
-point2 = 1
-endPoint = len(nums) - 1
+p1 = 0
+p2 = p1 + 1
 
-result = []
+while p2 < len(nums):
+    if nums[p1] == nums[p2]:
+        nums[p1] *=2
+        nums[p2] = 0
+    p1+=1
+    p2 +=1
 
-while point2 != len(nums) - 1:
-      if nums[point1] == nums[point2]:
-             nums[point1] *=2
-             nums[point2] = 0
-             nums[point2],nums[endPoint] = nums[endPoint],nums[point2]
-             point1 +=1
-             point2 +=1
-      elif nums[point1] != nums[point2]:
-           point2 +=1
-           point1 +=1
-count = 0
-for num in nums:
-      if num != 0:
-            result.append(num)
-      else:
-            count +=1
-
-while count  > 0:
-      result.append(0)
-      count -=1
+pos = 0
+for i in nums:
+    if i !=0:
+        nums[pos] = i
+        pos +=1
+        
+while pos < len(nums):
+    nums[pos] = 0
+    pos +=1
 
 print(nums)
-print(result)
-
-###i work on this like i am knowing the logic 
