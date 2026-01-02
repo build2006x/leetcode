2934b224526c4b270122bf26ccd7524b9692abd5
barath kumar <mabarathkumar@gmail.com>
2026-01-02 21:58:33 +0530

    de 30 and 02 jan update

---
+++ 02-01-2026.py
@@ -0,0 +1,12 @@
+##today i am doing the striver starting from today 
+##like i am starting my dsa jounery 
+
+
+##first i undergo some topic on the basic of the progammin language 
+
+## now i am going to pratice the patterns now before starting the dsa 
+
+###patern 
+
+for i in range(0,6):
+    print("*"*i)
---
+++ 30-12-2025.py
@@ -0,0 +1,15 @@
+## today i am starting slowing of the dsa 
+## i am starting with the queues  and stack 
+
+### to implementing with the collection module in the python 
+
+from collections import deque
+
+nums = deque()
+
+nums.append(10)
+nums.append(102)
+
+print(nums.popleft())
+
+
