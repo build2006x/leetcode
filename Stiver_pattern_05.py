5a4cee4f78d4e6bc5200d799a464ac25136ba328
barath kumar <mabarathkumar@gmail.com>
2026-01-04 11:58:55 +0530

    striver next pattern problem

---
+++ Stiver_pattern_05.py
@@ -0,0 +1,19 @@
+### this is problem i dealing with pattern printing 
+### 
+
+##think how many lines and what changes and what remain same 
+count = 5
+
+for i in range(0,5):
+    for j in range(0,count):
+        print("*",end="")
+    print("")
+    count -=1
+
+
+
+
+
+
+
+
