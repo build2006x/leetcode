479663889c7e714b75d0fe62da10db0bf110cc30
barath kumar <mabarathkumar@gmail.com>
2026-01-04 09:05:03 +0530

    stirver coding patterns

---
+++ 03-01-26.py
@@ -0,0 +1,9 @@
+### following the striver dsa sheet 
+### like in the stage of practing the 
+### pattern sums in the striver to build my approach solving and thinking 
+
+for i in range(0,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+        
