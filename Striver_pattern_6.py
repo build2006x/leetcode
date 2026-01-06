d6114dd6f6f0c6e5466c0b5b6b6c1b62b785b0b6
barath kumar <mabarathkumar@gmail.com>
2026-01-06 21:41:49 +0530

    pattern problem in the strivers dsa sheet

---
+++ Striver_pattern_6.py
@@ -0,0 +1,13 @@
+### today i need to work on the strivers dsa sheet to work on hard enough to 
+### to master the dsa 
+
+
+nums = ["1","2","3","4","5"]
+
+for i in range(0,len(nums)):
+    result = "".join(nums)
+    print(result)
+    nums.pop()
+    result = ""
+
+
