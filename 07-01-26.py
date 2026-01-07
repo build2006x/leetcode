7f0225bc62ae5eca525d1557281d1c76a260439e
barath kumar <mabarathkumar@gmail.com>
2026-01-07 23:16:31 +0530

    strivers pattern 7

---
+++ 07-01-26.py
@@ -0,0 +1,5 @@
+##today i encountering with the striver dsa sheet
+
+for i in range(1,11,2):
+    space = (9-i)//2  ###this is main logic where i am now i learned  
+    print(" " * space + "*" * i)
