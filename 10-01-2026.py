5498163ffc3fc7a371db7037d4b11f7c8a999c0c
barath kumar <mabarathkumar@gmail.com>
2026-01-10 09:32:08 +0530

    strivers dsa sheet

---
+++ 10-01-2026.py
@@ -0,0 +1,11 @@
+### today i doing with strivers pattern  to learning towards the dsa 
+
+start = 1
+
+for i in range(1,6):
+    for j in range(i):
+        print(start,end=" ")
+        start +=1
+    print("")
+
+
