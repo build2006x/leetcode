f7d0e8bc5d073929c1b545580b1aa9817935082a
barath kumar <mabarathkumar@gmail.com>
2026-01-10 09:41:47 +0530

    10-01-2026

---
+++ 10-01-2006.py
@@ -0,0 +1,12 @@
+### strivers dsa sheet pattern 
+### abc 
+
+result = ""
+
+arr = ["A","B","C","D"]
+start = 0      
+for i in range(1,6):
+     for i in range(1,i):
+          print(arr[start],end=" ")
+     print("")
+     start +=1     
