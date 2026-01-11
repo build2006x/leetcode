9df408ff82dfffba25cd4c77b42c7b8db3d4348e
barath kumar <mabarathkumar@gmail.com>
2026-01-11 09:45:30 +0530

    strivers dsa pattern 2 today

---
+++ 11strivers-02.py
@@ -0,0 +1,10 @@
+#### striver second today 11 date 
+
+arr = ["A","B","C","D","E"]
+start = 0
+
+for i in range(1,6):
+    for j in range(0,i):
+        print(arr[start],end="")
+    print("")
+    start +=1
