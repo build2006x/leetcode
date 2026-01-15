f86affcd02826465bdf5bb9566293df8146383d4
barath kumar <mabarathkumar@gmail.com>
2026-01-16 00:34:39 +0530

    recursion 1 to n times

--- recursion_strivers-2.py
+++ recursion_strivers-2.py
@@ -1,1 +1,14 @@
-### today i am doing the second sum
+### today i am doing the second
+### recursion call structure 
+
+## base function 
+## recursive call 
+
+def recursion(n,count):
+    if n == 0:
+        return ""
+    print(count,end=",")
+    return recursion(n-1,count+1)
+
+result = recursion(n=4,count=1)
+print()
