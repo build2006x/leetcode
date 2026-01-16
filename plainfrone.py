ac8191dce2128fa0332c36d35db609af0bc0ebac
barath kumar <mabarathkumar@gmail.com>
2026-01-16 08:41:59 +0530

    two pointer appraoch for the plaindrome string

--- plainfrone.py
+++ plainfrone.py
@@ -9,4 +9,22 @@ def plaindrome(string):
      return string[::-1] == string
 
 result = plaindrome(string="TAKE U FORWARD")
+
+
+### inplace two pointer approach 
+string = "ABCDCBA"
+
+
+def plaindromePointer():
+        start = 0
+        end = len(string)-1
+        while start != end:
+            if string[start] == string[start]:
+                     start +=1
+                     end -=1
+            else:
+                   return False 
+        return True
+
+result = plaindrome(string="TAKE U FORWARD")
 print(result)
