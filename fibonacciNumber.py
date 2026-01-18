13d72633383b2c8dc5be92c9d3fb3885c6de10df
barath kumar <mabarathkumar@gmail.com>
2026-01-18 15:44:23 +0530

    fibonacci number

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -1,6 +1,15 @@
 ### today i finishing the recursion in the strivers dsa sheet 
-
 ### recursion last topic - fibonacci number 
 N = 5
 
 #### brute force approach 
+def fibonaciNumber(N):
+    for i in range(0,N):
+        if (i + i+1) != (i+2):
+            return False
+    return True 
+
+result = fibonaciNumber(N = 5)
+print(result)
+
+
