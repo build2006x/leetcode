9e63a5fa6e8167dd3f0e040168ea156f23440608
barath kumar <mabarathkumar@gmail.com>
2026-01-18 16:16:17 +0530

    fibnonaaci series

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -1,15 +1,22 @@
 ### today i finishing the recursion in the strivers dsa sheet 
 ### recursion last topic - fibonacci number 
-N = 5
+### finding the series up to n terms in the fibonacci series 
 
-#### brute force approach 
+# def fibonaciSeries(N):
+#     for i in range(0,N):
+#         if (i + i+1) != (i+2):
+#             print(i,i+1,end="")
+
+# result = fibonaciSeries(N = 5)
+
+#### brute force approach  to finding the fibonaci number or not
 def fibonaciNumber(N):
-    for i in range(0,N):
-        if (i + i+1) != (i+2):
-            return False
-    return True 
+    n1,n2 = 0,1
+    for i in range(N+1):
+        print(n1, end=" ")
+        n1 , n2 = n2 , n1 + n2 
 
-result = fibonaciNumber(N = 5)
-print(result)
+fibonaciNumber(6)
+
 
 
