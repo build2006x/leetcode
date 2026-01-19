168a896dc858f893a85f9888048bf16b67e972fc
barath kumar <mabarathkumar@gmail.com>
2026-01-19 14:39:45 +0530

    fibonacci serires using recursion approach

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -10,13 +10,25 @@
 # result = fibonaciSeries(N = 5)
 
 #### brute force approach  to finding the fibonaci number or not
-def fibonaciNumber(N):
-    n1,n2 = 0,1
-    for i in range(N+1):
-        print(n1, end=" ")
-        n1 , n2 = n2 , n1 + n2 
+# def fibonaciNumber(N):
+#     n1,n2 = 0,1
+#     for i in range(N+1):
+#         print(n1, end=" ")
+#         n1 , n2 = n2 , n1 + n2 
 
-fibonaciNumber(6)
+# fibonaciNumber(6)
 
+##sturture of the recursion function 
+###  
+### 1.main function calling 
+### 2.base case for failing occurs 
+### 3.recursive calls 
+def fibonacciSeries(N,n1,n2):
+    if N == 0:
+        return ""
+    print(n1,end=" ")
+    return fibonacciSeries(N-1,n1=n2,n2=n1+n2)
+result = fibonacciSeries(N=5+1,n1=0,n2=1)
+print(result)
 
 
