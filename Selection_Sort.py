fed4cb32f73666b77c44a48e5d4f2502396b1a54
barath kumar <mabarathkumar@gmail.com>
2026-01-29 22:22:18 +0530

    selection sorting

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -4,13 +4,22 @@
 
 arr = [1,3,4,2,5]
 
-static = 0
-min_value = 0 
-
+static  = 0
+### selection sorting is about parition of the two parts where unsorted and sorted ones in the given array 
 while static < len(arr):
-      for i in range(static+1,len(arr)):
-              min_value = min(min_value,arr[i])
-      arr[static] = min_value
-      static +=1
+       min_value = static
+       for i in range(static+1,len(arr)):
+              if arr[static] > arr[i]:
+                      min_value = i
+       arr[static],arr[min_value] = arr[min_value],arr[static]
+       static +=1
 
 print(arr)
+
+
+### the point is keep tracker of the values and min values holder from the start of array 
+
+### logic -- where keeping two pointer - travler - Startindex - min_value 
+### travler variable will start moving from the Startindex
+
+### if large means get the index and swaps with the starting index
