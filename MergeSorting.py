8dcf4933065b304a8545bad1893c94ca0c0d8d77
barath kumar <mabarathkumar@gmail.com>
2026-02-05 19:14:06 +0530

    working on insertioSOrtRecrusion

--- MergeSorting.py
+++
@@ -1,6 +0,0 @@
-## today i am working on merge sorting algorithm 
-
-### -- we have to divide the array repeatedly and sort them and agin at last we have to merge them 
-
-arr = [4, 2, 2, 8, 4]
-
--- insertionSorting.py
+++ insertionSorting.py
@@ -2,7 +2,7 @@
 ### -- the point is all if not left element is not there we dont wanted to check 
 ### -- is the left element is being there means we have to sort them
 
-nums = [5, 4, 3, 2, 1] 
+nums = [5, 4,2,1] 
 
 for i in range(0,len(nums)-1):
     for j in range(0,len(nums)-1):
@@ -12,6 +12,20 @@ for i in range(0,len(nums)-1):
 print(nums)  
 
 
+### i am getting understaing much more of the outer loop in the above code where we leave a edge case of 0 to len(nums1)-1
+### like they are 4 elements means each pass of outer loop we know one element will defintely will placed corretly in the right position 
+### so its better to go for how many elements are being in the give array 
+
+### let me do this insertion sorting in the recursion approach....
+
+###---
+
+
+
+
+      
+      
+     
 
 
 
