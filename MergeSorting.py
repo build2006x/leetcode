232de7611f9c4d80ab90fd5ae64889b2f469118d
barath kumar <mabarathkumar@gmail.com>
2026-02-08 08:33:11 +0530

    general commit msg

--- MergeSorting.py
+++ MergeSorting.py
@@ -24,8 +24,6 @@ def mergeSort(arr):
         point1 = 0
         point2 = 0
         track = 0 #### this varaiable which is starting off index to put our values in arr by compare the left and right 
-        print(f"leftarray:{left_half}")
-        print(f"rightarray:{righ_half}")
         #### here we are setting the two pointer to go through and compare and sort the array 
         while point1 < len(left_half) and point2 < len(righ_half):
               if left_half[point1] < righ_half[point2]:
