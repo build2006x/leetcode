0e2b5b96443b4c91f24f82b03a000e59bb3a4f06
barath kumar <mabarathkumar@gmail.com>
2026-02-14 07:28:43 +0530

    optimal approach is being done for the rotating the arr

--- LeftRotate.py
+++ LeftRotate.py
@@ -1,12 +1,26 @@
 ### today i am working on the left rotate array by 1 
 
+# nums =  [-1, 0, 3, 6]  
+# first= nums[0]
+
+# for i in range(0,len(nums)-1):
+#      nums[i] = nums[i+1]
+#      if i == len(nums)-2:
+#           nums.pop()
+#           nums.append(first)
+
+# print(nums)
+
+
+### anothor brute force 
 nums =  [-1, 0, 3, 6]  
-first= nums[0]
+result = []
 
-for i in range(0,len(nums)-1):
-     nums[i] = nums[i+1]
-     if i == len(nums)-2:
-          nums.pop()
-          nums.append(first)
+for i in range(1,len(nums)):
+      result.append(nums[i])
+result.append(nums[0])
+print(result)
 
-print(nums)
+
+#### this is the brute force method above now we explore the 
+#### the main optimal way of doing above problem 
