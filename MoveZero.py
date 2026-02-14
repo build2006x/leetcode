2f77257204effba4d68703f70a4b45c07a820185
barath kumar <mabarathkumar@gmail.com>
2026-02-14 08:55:23 +0530

    moving the zero to the end is done

--- MoveZero.py
+++ MoveZero.py
@@ -1,14 +1,17 @@
 #### today i working on the moving the zero to the end leetcode sum 
 nums = [1 ,0 ,2 ,3 ,0 ,4 ,0 ,1]
 
-point1 = 0
 
-for i in range(0,len(nums)-1):
-     if nums[i] == 0 and nums[i+1] ==0:
-            nums[i],nums[i+2] = nums[i+2],nums[i]    
-     elif nums[i] == 0:
-         nums[i],nums[i+1] = nums[i+1],nums[i]
-     print(nums)
+### first let me done with brute force 
+result = []
+count = 0
+for i in range(0,len(nums)):
+    if nums[i] != 0:
+        result.append(nums[i])
+    elif nums[i] == 0:
+        count +=1
 
-#### here the edge case about double zero in the row 
+for j in range(0,count):
+    result.append(0)
 
+print(result)
