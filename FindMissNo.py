fa5e9391015f639c21fa050449a9f4a03c23a9b9
barath kumar <mabarathkumar@gmail.com>
2026-02-16 18:18:23 +0530

    finding the missing no in the array

---
+++ FindMissNo.py
@@ -0,0 +1,9 @@
+### simple question is to finding the missing the number in the array nput: 
+
+arr = [8, 2, 4, 5, 3, 7, 1]
+
+for i in range(1,len(arr)):
+     if i not in arr:
+          print(i)
+
+### this is the simple brute force we can solve them in the minute
--- UnionElement.py
+++ UnionElement.py
@@ -1,16 +1,19 @@
 ### i wanted to find the common element 
 
-n = 5
-m = 5 
-arr1 = [1,2,3,4,5]  
-arr2 = [2,3,4,4,5]
+# n = 5
+# m = 5 
+# arr1 = [1,2,3,4,5]  
+# arr2 = [2,3,4,4,5]
 
-setMap  = set()
+# setMap  = set()
 
-for i in range(0,len(arr1)):
-    setMap.add(arr1[i])
+# for i in range(0,len(arr1)):
+#     setMap.add(arr1[i])
 
-for j in range(0,len(arr2)):
-    setMap.add(arr2[i])
+# for j in range(0,len(arr2)):
+#     setMap.add(arr2[i])
 
-print(setMap)
+# print(setMap)
+
+### above code belong to the how to get the union from the sorted arr using the 
+### brute force approach 
