85cc9a3a0d47280190e0f446e52f7aa1d2bbce22
barath kumar <mabarathkumar@gmail.com>
2026-02-17 11:48:26 +0530

    general commit

--- Maxiconsecutive.py
+++ Maxiconsecutive.py
@@ -1,27 +1,22 @@
 ### today i am working on the consecutive ones in the array 
 
-prices = [1, 1, 0, 1, 1, 1]
-
-
-
 ### basic apporach like i am thinking in the perspective of count the value and zero them and check the value greater means 
 ### take them orelse leave them 
+
+prices = [1, 1, 0, 1, 1, 1]
+
 track = 0
-max_value = 0
-count = 0
+point = 0
+value = 0
 
-while track < len(prices):
-      while track < len(prices):
-            if prices[track] == 1:
-                   count +=1 
-            else:
-                  break 
-      track +=1
-      max_value = max(max_value,count)
-      count = 0
-      if count == len(prices) - 1:
-            break
+while point < len(prices):
+      if prices[point] == 1:
+             track +=1
+             point +=1
+      else:
+           value = max(value,track)
+      point +=1
 
-print(max_value)
-
-            
+print(value)
+       
+      
