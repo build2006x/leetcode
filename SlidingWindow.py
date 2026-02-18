c288095aa9dac1e3e0e220f504d2f8f5afa99abd
barath kumar <mabarathkumar@gmail.com>
2026-02-18 18:04:30 +0530

    sliding window getting started

---
+++ SlidingWindow.py
@@ -0,0 +1,12 @@
+### today i am learning the one of the important pattern in the leetcode 
+### as back i am learned the two pointer problem 
+### now i am moving to the sliding window concept 
+
+arr1 = [42, 17, 56, 23, 89, 12]
+k  = 3
+
+for i in range(len(arr1)-1):
+     print(arr1[i:i+k])
+
+### this is how i am thinking to print the element using a fixed 
+### window size where the window size will be matined in the printing statment
