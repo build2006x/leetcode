67f85b3e1e7a0359c47e9c4952dfc3dbd2f48d74
barath kumar <mabarathkumar@gmail.com>
2026-02-18 11:08:19 +0530

    solved using hasing

--- FindCount.py
+++ FindCount.py
@@ -1,13 +1,31 @@
 #### today i working on the sum where i need to find the number which have 1 count 
 
-from collections import Counter
+# from collections import Counter
 
-arr = [2,2,1]
+# arr = [2,2,1]
 
-Count = Counter(arr)
+# Count = Counter(arr)
 
-for i,j in Count.items():
-    if j == 1:
-        print(i)
+# for i,j in Count.items():
+#     if j == 1:
+#         print(i)
 
 #### here we are using the frequency module to get the count of the each element
+
+arr = [4,1,2,1,2]
+
+arr_final  = [0] * len(arr)
+
+for i in arr:
+    if i in arr_final:
+            arr_final[i] +=1
+    else:
+          arr_final[i] +=1
+
+for i in range(0,len(arr)):
+      if arr_final[i] == 1:
+            print(i)
+
+#### using hasing solved the above problem 
+### two values -- one is store the pre define zero set to update them 
+### later by mapping the value to the array 
