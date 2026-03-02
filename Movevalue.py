b4b561da1de59944654a0be25d9cbf656229cdde
barath kumar <mabarathkumar@gmail.com>
2026-03-02 12:30:17 +0530

    move the value to the end return the correct lenght of the array

--- Movevalue.py
+++ Movevalue.py
@@ -1,20 +1,23 @@
 ### i am working on the sum which have to remove the the front  the remaining values
-nums = [0,1,2,2,3,0,4,2] 
-val = 2
-
+nums = [2, 0, 2, 1, 2, 3] 
+val = 2 
 point1 = 0 
 point2 = len(nums) - 1
 count = 0 
 
 while point1 < point2:
-     if nums[point1] == val and nums[point2] != val:
-            nums[point1],nums[point2] = nums[point2],nums[point1]
-            point1 +=1
-            point2 -=1
-     elif nums[point1] == val and nums[point2] == val:
-            point2 -=1
+      if nums[point1] == val and nums[point2] != val:
+             nums[point1],nums[point2] = nums[point2],nums[point1]
+             point1 +=1
+             point2 -=1
+             count +=1
+      elif nums[point1] !=val:
+             point1 +=1
+      elif nums[point1] == val and nums[point2] == val:
+             point2 -=1
+             count +=1
 
-
-
+print(nums[0:len(nums)-count])
+print(count)
 
         
