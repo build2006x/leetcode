d666f1a2e589b2b0db8a93872582bd9041de86b1
barath kumar <mabarathkumar@gmail.com>
2026-03-04 18:36:58 +0530

    approach is not working still working on that parentheses sum

--- VaildParentheses.py
+++ VaildParentheses.py
@@ -1,18 +1,29 @@
 string = "{[()]}"
 
-open = "{[(])}"
+# open = "{[(])}"
 
-open_result = []
-close_result = []
+# open_result = []
+# close_result = []
+# close = [')','}',')']
 
+# for i in string:
+#     if i in close:
+#         close_result.append(i)
+#     else:
+#         open_result.append(i)
 
-close = [')','}',')']
+# print(len(open_result)==len(close_result))
+### this above way of approach is not working out 
+
+string = "((())"
+
+arr_dict  = {'[':']','{':'}','(':')'}
+stack = []
 
 for i in string:
-    if i in open:
-        open_result.append(i)
-    else:
-        close_result.append(i)
-
-
-print(len(open_result)==len(close_result))
+    if i in arr_dict:
+        stack.append(i)
+        if arr_dict[i] in string:
+             stack.pop()
+    
+print(stack)
