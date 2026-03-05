be59e2cc544975a4aa5f028c2df3e5365e6afac4
barath kumar <mabarathkumar@gmail.com>
2026-03-05 12:26:36 +0530

    completed the remove the elemenet in leetcode

--- Movevalue.py
+++ Movevalue.py
@@ -1,15 +1,12 @@
 ### i am working on the sum which have to remove the the front  the remaining values
 nums = [3,3]
 val = 3
+read = 0
 
-read  = 0
+for i in range(0,len(nums)):
+    if nums[i] != val:
+        nums[read] = nums[i]
+        read +=1
 
-
-for i in nums:
-    if nums[i] == val:
-         nums[read]= nums[i]
-         read +=1
-      
 print(nums[0:read])
-
         
