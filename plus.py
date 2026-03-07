fb4f3fa68058e0775e4d0226cb5575e2fa4b4ae9
barath kumar <mabarathkumar@gmail.com>
2026-03-07 11:42:24 +0530

    plus one sum leetcode is solved

--- plus.py
+++ plus.py
@@ -1,16 +1,27 @@
 ### today 
 ### i am working on the plus one sum 
 
-digits = [9]
+digits = [9,9]
 main = digits[::-1]
-p1 = 0
+read = 0
 
-while p1 < len(digits):
-     if main[p1] == 9:
-           main[p1] = 0 
-           main[p1+1] +=1  
-     main[p1] +=1
-     break
+### the logic perfectly works like if the 9 is there put the zero until up and next pointer increment them if not end the read means append a one 
 
-print(main[::-1])
+if main[read] != 9:
+       main[read] +=1
+       print(read[::-1])
+else:
+    while read < len(digits) and main[read] ==9:
+          main[read] = 0
+          read +=1
+    if read < len(digits):
+           main[read] +=1
+    else:
+         main.append(1)
+         print(main[::-1]) 
+        
+  
+
+
+
  
