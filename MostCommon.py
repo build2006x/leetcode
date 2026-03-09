984ddb7ef31a6be1aac30d5401aa8b9f13fdace6
barath kumar <mabarathkumar@gmail.com>
2026-03-09 06:01:39 +0530

    come with brute force to solve this problem

--- MostCommon.py
+++ MostCommon.py
@@ -1,19 +1,25 @@
 ### hey hi da this leetcode sum is to find the most common words in the string not banned names 
 
-paragraph = "Bob hit a ball, the hit BALL flew far after it was hit." 
+para = paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.".lower().replace(".","")
 banned = ["hit"]
-arr = paragraph.split(" ")
 
+arr = para.split(" ")
 
 arrs = [0,3,2,4,4,25,24]
-
 fre = {}
 
-for i in arrs:
-    if i in fre:
+for i in arr:
+    if i in fre and i !=banned:
         fre[i] +=1
     else:
         fre[i] = 1
 
+print(fre)
 
-    
+# print(fre)
+# max_idx = 0
+
+# for val,idx in fre.items():
+#     if val 
+
+
