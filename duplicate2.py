34767a7a1c5dd0da1dfdfcd8935b19e7b7daf0c3
barath kumar <mabarathkumar@gmail.com>
2026-03-13 23:33:25 +0530

    thinking in the sliding window techinque to solve this problem

--- duplicate2.py
+++ duplicate2.py
@@ -14,18 +14,7 @@
 ### like we can use the approach of the sliding window 
 
 def findDupe(nums,k):
-    left = 0
-    rigth = 2
-    while left < len(nums) - 1:
-        sumVal =rigth - left
-        if nums[left] == sumVal:
-            if abs(left + rigth) <= k:
-                return True
-        sumVal = 0
-        rigth +=1
-        left +=1
-    return False     
- 
-result = findDupe( nums = [1,0,1,1], k = 1)
+    return ""
+result = findDupe(nums = [1,0,1,1], k = 1)
 
 print(result)
