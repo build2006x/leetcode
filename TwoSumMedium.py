e51ca365ebbaf836416460a93ccaf6d71f524ae0
barath kumar <mabarathkumar@gmail.com>
2026-03-15 18:38:05 +0530

    trying to come with optimal approach for two sum

--- TwoSumMedium.py
+++ TwoSumMedium.py
@@ -2,19 +2,26 @@
 
 ### i am using the 
 
-arr = [2,6,5,8,11]
-target = 15
-left = 0
-right = 2
+# arr =  [2,6,5,8,11]
+# target = 14
+# left = 0
+# right = 2
 
-while left < len(arr)-1:
-    while right < len(arr):
-         if arr[left]+arr[right] == target:
-               print('yes')
-         right +=1
-    left +=1
-    right = left + 1
+# while left < len(arr)-1:
+#     while right < len(arr):
+#          if arr[left]+arr[right] == target:
+#                print(left,right)
+#          right +=1
+#     left +=1
+#     right = left + 1
 
 
         
-          
+arr = [2,6,5,8,11]
+target = 15
+
+left = 0
+right = 2 
+
+for i,num in enumerate(arr):
+    print(i,num)
