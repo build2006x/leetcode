484214d6fadb6ec36b5e6992c520f4f86b08ed32
barath kumar <mabarathkumar@gmail.com>
2026-04-07 22:14:30 +0530

    best time to buy and sell ..trying to make the two loop one

--- BestTimeBuy.py
+++ BestTimeBuy.py
@@ -2,23 +2,37 @@
 ### i am working on the best time to buy the stock and sell them 
 ### -- output - 5 
 
-prices = [7,6,4,3,1]
+# prices = [7,6,4,3,1]
+# min_entry = prices[0]
+# idx_min = 0
+# max_entry = 0
 
-min_entry = prices[0]
-idx_min = 0
-max_entry = 0
+# for idx,val in enumerate(prices):
+#     if val < min_entry:
+#         min_entry = val
+#         idx_min = idx
 
-for idx,val in enumerate(prices):
-    if val < min_entry:
-        min_entry = val
-        idx_min = idx
+# for n in range(idx_min+1,len(prices)):
+#     if prices[n] > max_entry:
+#         max_entry = prices[n]
 
-for n in range(idx_min+1,len(prices)):
-    if prices[n] > max_entry:
-        max_entry = prices[n]
-
-print(max_entry-min_entry)
+# print(max_entry-min_entry)
 
 ### above code use the burte force to sovle the problem ...now we think to solve the problem using the any pattern 
 
 ### likely the two pointer or sliding window 
+
+
+### likely today i wanted to try some other way to solve the problem of the best time to buy the stock 
+
+prices = [7,6,4,3,1]
+
+market_entry  = prices[0] 
+market_exit   = 0
+
+for num in range(1,len(prices)):
+    market_entry = min(market_entry,prices[num])
+    market_exit = max(market_exit,prices[num])
+
+
+print(market_exit)
