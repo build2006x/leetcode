184695dfcfb0875ac236e5e4b6e6b2b7ace4da08
barath kumar <mabarathkumar@gmail.com>
2026-04-20 10:05:36 +0530

    working on the permutation leetcode problem

---
+++ permutation.py
@@ -0,0 +1,7 @@
+#### today i am working on the permutation problem on the leetcode 
+### normally we going to use the recurssion as the approach to this problem means off 
+### three case we have in the recurssion -- base_case -- main logic -- recurssive call  
+
+
+arr = [1, 2, 3]
+
