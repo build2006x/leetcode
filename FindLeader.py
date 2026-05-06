36f0285bfbde1c13fb80ebccdc8ae9e757453cf2
barath kumar <mabarathkumar@gmail.com>
2026-05-06 09:10:14 +0530

    solved the finding the leader in the array leetcode sum

---
+++ FindLeader.py
@@ -0,0 +1,20 @@
+#### today i am working on the leetcode problem ---finding the leader 
+arr = [10, 22, 12, 3, 0, 6]
+reader = 0
+pointer = 1
+result = []
+
+while reader < len(arr):
+      while pointer < len(arr): 
+         if arr[reader] > arr[pointer]:
+              pointer +=1
+         else:
+              break 
+      if pointer == len(arr):
+           result.append(arr[reader])
+      reader +=1
+      pointer = reader + 1 
+     
+
+print(result)
+            
---
+++ weeklyContest.py
@@ -0,0 +1,12 @@
+### today weekly contest -- 26/4/26
+nums = [1,2,4,2,3,2]
+
+pointer =  1
+
+result = []
+nums.append(nums[0])
+
+for i in range(1,len(nums)-2):
+    
+result.append(nums[-1])
+
