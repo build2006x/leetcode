d53d3e5a835060987026971704c49840cd4cf268
barath kumar <mabarathkumar@gmail.com>
2026-05-07 18:54:38 +0530

    sovling the martix problem

---
+++ Matrix.py
@@ -0,0 +1,20 @@
+### hi today i am working on the matrix sum 
+### if the array contains a 0 means it should be 0 all rows and columns 
+
+matrix=[
+        [1,1,1],
+        [1,0,1],
+        [1,1,1]
+]
+
+
+            
+
+
+
+
+
+
+
+
+
