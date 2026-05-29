c9148e37da87e59beee613209953ab315130890b
barath kumar <mabarathkumar@gmail.com>
2026-05-29 22:16:17 +0530

    solved the zero matrix using brute force

--- Matrix.py
+++ Matrix.py
@@ -1,25 +1,28 @@
 ### hi today i am working on the matrix sum 
 ### if the array contains a 0 means it should be 0 all rows and columns 
 
+#### the solution == brute force 
+### if one find means --- change it as to -1 and again iterate over the array --- change the -1 into 0 
+
 matrix=[
-        [1,1,1],
+        [1,1,0],
         [1,0,1],
         [1,1,1]
 ]
 
 for i in matrix:
-    for j in range(0,1):
-        print(i[j],i[j+1],i[j+2])
+    for idx,j in enumerate(i):
+      if j == 0:
+        for k in range(0,3):
+            i[k] = -1
+            for q in matrix:
+                q[idx] = -1 
 
-
-
-
-
-
-
-
-
-
-
-
+for i in matrix:
+    for idx,j in enumerate(i):
+       if j == -1:
+         i[idx] = 0
+         
+for i in matrix:
+   print(i)
 
