8426d30b037ed8c49e0ee041045512a631973a3b
barath kumar <mabarathkumar@gmail.com>
2026-06-10 08:45:41 +0530

    solved the subarray problem using the brute force

--- 8.6.26.py
+++ 8.6.26.py
@@ -3,19 +3,14 @@
 array = [3,1, 2, 4]
 k=6
 
-left = 0
-right = 1
-count = 1
+for left in range(len(array)): ### keeping the left as a fixed lock ----
+    current_sum = 0
+    for right in range(left, len(array)): #### keep the left as fixed and expand the right forwards 
+        current_sum += array[right]
+        if current_sum == k:
+            print(array[left:right+1])
 
-while left < len(array):
-    while right < len(array):
-         if array[left:right+1] == k:
-              print(array[left],array[right])
-         right +=1
-    right = left + 1
-
-
-
+#### next i wanted to learn the siliding window technique ? 
       
       
       
