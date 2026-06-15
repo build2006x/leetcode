9c881118f48bff7c16d32bbc47a8026fea648f9d
barath kumar <mabarathkumar@gmail.com>
2026-06-15 22:42:59 +0530

    trying to solve the spiral printing

---
+++ 15.6.26.py
@@ -0,0 +1,7 @@
+### today i working again in the spiral printing pattern in the given array 
+
+Matrix = [[1, 2, 3, 4 ],[5, 6, 7, 8],[ 9, 10, 11, 12 ],[ 13, 14, 15, 16 ] ]
+
+
+
+
---
+++ Sql01.py
@@ -0,0 +1,2 @@
+#### today i feeling like to work on the sql query question 
+
