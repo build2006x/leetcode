86dd041674061c9bf40e7dd3607893794e121b09
barath kumar <mabarathkumar@gmail.com>
2026-06-21 10:13:54 +0530

    sovling the problem of finding the longestest prefix

---
+++ array2.py
@@ -0,0 +1,17 @@
+### today 21.6.26 , i am working on Write a function to find the longest common prefix string amongst an array of strings.
+
+strs = ["flower","flow","flight"]
+
+
+pointer = 0
+
+while True:  
+      if strs[0][pointer] == strs[1][pointer] == strs[2][pointer]:
+              pointer +=1
+      else: 
+            print(strs[0][pointer])
+            break
+      
+ 
+
+
