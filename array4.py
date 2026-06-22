71d7525a88490fc8d495bbf2c158061b013278d1
barath kumar <mabarathkumar@gmail.com>
2026-06-22 14:59:32 +0530

    solving the 3sum question

--- array4.py
+++ array4.py
@@ -6,16 +6,27 @@ nums = [-1, 0, 1, 2, -1, -4]
 pointer_one = 0
 pointer_two = 1
 pointer_three = 2
+main = 4
 
 
-count = 4
-while count != 0 :
-    if nums[pointer_one] + nums[pointer_two] + nums[pointer_three] == 0:
-        print([pointer_one,pointer_two,pointer_three])
-    pointer_one +=1
-    pointer_three +=1
-    pointer_two +=1
-    count -=1    
+while main !=0:
+    count = 4
+    while count != 0 and pointer_three < len(nums):
+        if nums[pointer_one] + nums[pointer_two] + nums[pointer_three] == 0:
+            print([nums[pointer_one],nums[pointer_two],nums[pointer_three]])
+        pointer_one +=1
+        pointer_three +=1
+        pointer_two +=1
+        count -=1
+    main -=1
+    count = 4
+    pointer_one = main + 1
+    pointer_two = pointer_one + 1
+    pointer_three = pointer_two +  1
+
+
+
+
             
 
 
