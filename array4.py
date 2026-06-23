060aa595b3eb77dcb2ea86a5b16460b971da4e09
barath kumar <mabarathkumar@gmail.com>
2026-06-23 10:19:38 +0530

    solved the 3sum problem using the two pointer method

--- array4.py
+++ array4.py
@@ -3,28 +3,22 @@
 
 nums = [-1, 0, 1, 2, -1, -4]
 
-pointer_one = 0
-pointer_two = 1
-pointer_three = 2
-main = 4
+fixed = 0
+left = fixed + 1
+right = 2
 
 
-while main !=0:
-    count = 4
-    while count != 0 and pointer_three < len(nums):
-        if nums[pointer_one] + nums[pointer_two] + nums[pointer_three] == 0:
-            print([nums[pointer_one],nums[pointer_two],nums[pointer_three]])
-        pointer_one +=1
-        pointer_three +=1
-        pointer_two +=1
-        count -=1
-    main -=1
-    count = 4
-    pointer_one = main + 1
-    pointer_two = pointer_one + 1
-    pointer_three = pointer_two +  1
+while fixed != 4:
+    while right < len(nums):
+          if nums[fixed] + nums[left] + nums[right] == 0:
+                print([nums[fixed],nums[left],nums[right]])
+          right +=1
+          left +=1
+    fixed +=1
+    left = fixed + 1
+    right = left + 1
 
-
+ 
 
 
             
