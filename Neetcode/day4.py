4a2dfc8a51acd81c13e9ea3f66014785d0b797ad
barath kumar <mabarathkumar@gmail.com>
2026-06-28 22:45:01 +0530

    trying the neetcode problem daily

---
+++ Neetcode/day4.py
@@ -0,0 +1,4 @@
+### today i working on the medium , but today i learned one thing in my life med hard easy its all illusion we make in our mind we can solve this much 
+## only but i always think that every problem have solution -- just keeping solving 
+
+
