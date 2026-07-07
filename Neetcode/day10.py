393ef1932daf4036e0d75005b992f3ac98acbfa7
barath kumar <mabarathkumar@gmail.com>
2026-07-07 20:56:21 +0530

    solved the medium level group anagram problem

--- Neetcode/day10.py
+++ Neetcode/day10.py
@@ -1,20 +1,25 @@
 pointer  = 0 
-move_pointer = 0
+move_pointer = pointer + 1
 result  = []
 
-strs=["act","pots","tops","cat","stop","hat"]
+strs = ["act","pots","tops","cat","stop","hat"]
 
 while pointer < len(strs):
-    result.append(strs[pointer])
-    while move_pointer < len(strs):    
-        for index,val in enumerate(strs[pointer]):
-            if val in strs[move_pointer]:
-                if index == len(strs[move_pointer]):
-                    result.append(strs[move_pointer])
-            else:
-               break
-        move_pointer +=1
-    move_pointer = 0
-    pointer +=1
+     result.append([strs[pointer]])
+     while move_pointer < len(strs):
+            if move_pointer != pointer:
+                   if sorted(strs[pointer]) == sorted(strs[move_pointer]):
+                        result[pointer].append(strs[move_pointer])
+            move_pointer +=1
+     move_pointer = 0
+     pointer +=1
 
-print(result)
+new_result = []
+
+for i in result:
+      new = sorted(i)
+      if new not in new_result:
+            new_result.append(new)
+      new = []
+    
+print(new_result)
