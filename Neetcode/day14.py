461c12d59cd77c4b83ead0340271b840418e7e20
barath kumar <mabarathkumar@gmail.com>
2026-07-16 22:22:05 +0530

    sovled the sum

--- Neetcode/day14.py
+++ Neetcode/day14.py
@@ -1,20 +1,15 @@
 flowerbed=[0,0,1,0,0,0,0,0,1,0,0]
-n=4
+n=4 
 
 
-pointer = 0
+new =  [0] + flowerbed  + [0]
 
-while pointer < len(flowerbed):
-     if flowerbed[pointer] == 0 and pointer < len(flowerbed)-1:
-          if flowerbed[pointer-1] and flowerbed[pointer+1] ==0:
-                         flowerbed[pointer] = 9
-          elif flowerbed[pointer+1]==0:
-                    if flowerbed[pointer] ==0:
-                         flowerbed[pointer] = 9
-          elif flowerbed[pointer+1] and flowerbed[pointer-1] ==0:
-                    if flowerbed[pointer] == 0:
-                         flowerbed[pointer] = 9
-     pointer +=1  
- 
+pointer = 1
 
-print(flowerbed)
+while pointer < len(new) - 1:
+          if new[pointer-1] ==0 and new[pointer+1] == 0 and new[pointer]  ==0:
+                                   new[pointer] = 1
+                                   n -=1
+          pointer +=1
+
+print(n<=0)
