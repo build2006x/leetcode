496c2636ebe0ee8922fb85baad973877c15a6ead
barath kumar <mabarathkumar@gmail.com>
2026-07-20 18:23:41 +0530

    trying to solve the problem

--- Neetcode/day16.py
+++ Neetcode/day16.py
@@ -5,45 +5,42 @@ nums = [1,4,3,3,2]
 l = 0
 r = 1
 start = 0
-end = 0
+reader = 1
 result = []
 
 while r < len(nums):
-        if nums[l] > nums[r]:
-            start = l
-            end = l + 1
-            r +=1
-            l +=1
-        else:
-             result.append(nums[start:end+1])  
-             l +=1
-             r +=1
-             start = 0
-             end = 0
+    print(nums[l],nums[r])
+    if nums[l] > nums[r] and nums[l] != nums[r]:
+        start = l
+        reader +=1
+        l +=1
+        r +=1
+    else:
+        result.append(nums[l:r+1])
+        l +=1
+        r +=1
+        start = 0
+        reader = 1
 
-left = 0
-right = 1
-starting = 0
-ending = 0
 
-# while right < len(nums):
-#         if nums[left] > nums[right]:
-#             starting = l - 1 
-#             ending = starting + 1
-#             r +=1
-#             l +=1
-#         else:
-#              result.append(nums[starting:ending+1])  
-#              l +=1
-#              r +=1
-#              satrt = 0
-#              end = 0
+# l = 0
+# r = 1
+# start = 0
+# reader = 1
+
+# while r < len(nums):
+#     if nums[l] < nums[r]:
+#         start = l 
+#         reader +=1
+#         l +=1
+#         r +=1
+#     else:
+#         result.append(nums[l:r+1])
+#         l +=1
+#         r +=1
+#         start = 0
+#         reader = 1
+
 print(result)
-
-
-
-
-
-
 
 
