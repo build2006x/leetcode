f2e6e7443d3e939c729e0f42d2ccb8180b5f498d
barath kumar <mabarathkumar@gmail.com>
2026-08-19 08:28:00 +0530

    sovled the lucky number problem

---
+++ Neetcode/day26-2.py
@@ -0,0 +1,14 @@
+### Find Lucky Integer in an Array
+
+### this is the problem right now i am working on 
+from collections import Counter
+
+arr = arr=[2,2,3,4]
+count = 0
+ar = Counter(arr)
+
+for fre,val in ar.items():
+    if fre == val:
+        
+
+print(count)
