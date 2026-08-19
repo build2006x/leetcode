e409faacf3d0b0724a02ca1e84e48eb660fff9cc
barath kumar <mabarathkumar@gmail.com>
2026-08-20 05:06:14 +0530

    understanding the question

---
+++ Neetcode/day27-2.py
@@ -0,0 +1,4 @@
+### Check if Array Is Sorted and Rotated today i am working on this problem 
+
+### the problem defintion is that i need check the array is non desecending order if not i need to use the formula to shift the array for every vaild index 
+### no shift cant be equal to the sorted array means return False 
