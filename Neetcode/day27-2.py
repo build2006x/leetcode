1bd072ec14b0129984b860f3af5a73b26e7f0172
barath kumar <mabarathkumar@gmail.com>
2026-08-23 09:21:02 +0530

    solved the array is being sorted after rotated x position

--- Neetcode/day27-2.py
+++ Neetcode/day27-2.py
@@ -1,4 +1,21 @@
 ### Check if Array Is Sorted and Rotated today i am working on this problem 
-
 ### the problem defintion is that i need check the array is non desecending order if not i need to use the formula to shift the array for every vaild index 
 ### no shift cant be equal to the sorted array means return False 
+
+
+nums =[1,2,3]
+pointer = 1
+roated_arr = []
+
+if sorted(nums) == nums:
+    print('true')
+
+
+while pointer < len(nums):
+   if sorted(nums) == roated_arr:
+      print('True')
+      break
+   else:
+        roated_arr = nums[pointer:] + nums[:pointer]
+        pointer +=1
+
