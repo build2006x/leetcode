6990d8a350e696e96489e03bf499f8432908e00a
buildx2006 <mabarathkumar@gmail.com>
2026-08-26 09:13:44 +0530

    check the from the char it could form the word

--- Neetcode/day28-4.py
+++ Neetcode/day28-4.py
@@ -1,27 +1,14 @@
 ### hey hi today i am working on the Find Words That Can Be Formed by Characters
-
+from collections import Counter
 words = ["cat","bt","hat","tree"]
-
 chars = "atach"
-
-
-static = 0 
-reader = 0 
-p1 = 0
 result = 0
+char_count = Counter(chars)
 
-while p1 < len(words):
-        while reader < len(chars) and static < len(words[p1]):
-                if words[p1][static] == chars[reader]:
-                            static +=1
-                            reader  +=1
-                else:
-                    reader +=1
-        if static == len(words[p1])-1:
-                result += len(words[p1])
-        static = 0
-        reader  = 0
-        p1 +=1
+
+for word in words:
+     word_count = Counter(word)
+     if all(word_count[c] <= char_count[c] for c in word):
+           result += len(word)
+
 print(result)
-
-
