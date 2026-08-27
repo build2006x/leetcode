f99c823b555bfef12cc1320524de694ce3a36f10
buildx2006 <mabarathkumar@gmail.com>
2026-08-27 22:07:35 +0530

    tried the neetcode problem

--- Neetcode/day31-2.py
+++ Neetcode/day31-2.py
@@ -1,27 +1,27 @@
 ### Largest 3-Same-Digit Number in String (problem)
 ### today august 27 --- 2026
 ### problem goals is to return the longest same integer from integer as string 
-num="2300019"
+num="112233445566778899000"
 
 static = 0
 dynamic = 1
 pointer = 0
 result = ""
-max_char = 0
+sol = []
 
 while pointer < len(num):
         if len(num[static:dynamic+1]) == 3 and num[dynamic] == num[dynamic-1]:
                 result = num[static:dynamic+1]
-                print(max(0,int(result)))
-                break
-                max_char = max(int(result),max_char)   
+                sol.append(result)        
                 static = dynamic + 1
                 dynamic = static + 1
         elif dynamic < len(num) and num[static] == num[dynamic]:
+                print(static)
                 dynamic +=1
         else:
             static +=1
             dynamic = static + 1
         pointer +=1
      
-print(max_char)
+
+print(sol)
