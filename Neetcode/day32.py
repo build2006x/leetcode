e21083c872270239be5e6e5cd9ecd853009ae610
buildx2006 <mabarathkumar@gmail.com>
2026-08-28 14:55:43 +0530

    got the first thining logic

---
+++ Neetcode/day32.py
@@ -0,0 +1,6 @@
+### hey hi today i am working on the 1436. Destination City
+
+### question we wanted to find the city which doesnt have destination 
+
+### what logic did think in my mind--
+### ---> find all city and check which doesnt have the right side part and choose that one and return back
