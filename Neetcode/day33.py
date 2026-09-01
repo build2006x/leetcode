98cb1bd9f0a84c25578113e96d68e07e077e812a
buildx2006 <mabarathkumar@gmail.com>
2026-09-01 21:01:44 +0530

    felling not well today so written the brute force algorithm

---
+++ Neetcode/day33.py
@@ -0,0 +1,9 @@
+### hey hi today i am working on the 
+##Maximum Product Difference Between Two Pairs
+
+"""
+algorithm****
+1.take one pair 
+2.before compare with all other same pair would not be compared 
+3.compute each pair of the sum and store in the array return the max one 
+"""
