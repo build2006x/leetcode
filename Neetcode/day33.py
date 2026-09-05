feab89bb623e76320b6968700e2dc7ba7ffde667
buildx2006 <mabarathkumar@gmail.com>
2026-09-05 05:59:41 +0530

    done

--- Neetcode/day33.py
+++ Neetcode/day33.py
@@ -8,3 +8,14 @@ algorithm****
 3.compute each pair of the sum and store in the array return the max one 
 """
 nums = [5,6,2,7,4]
+
+first = 0 
+second =  first + 1
+last = -1
+last_before = -2 
+
+nums_sorted = sorted(nums)
+
+ans = (nums_sorted[last] * nums_sorted[last_before]) - (nums_sorted[first] * nums_sorted[second])
+
+print(ans)
