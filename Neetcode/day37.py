e9457f264df3b1fa815f8545dec8d3d3d0b312e9
buildx2006 <mabarathkumar@gmail.com>
2026-09-12 12:26:56 +0530

    solved the merge 2d arr

--- Neetcode/day37.py
+++ Neetcode/day37.py
@@ -1,10 +1,10 @@
 ####### hey hi today i am working 
 ####### on the Merge Two 2D Arrays by Summing Values
 
-
 nums1 = [[1,2],[2,3],[4,5]]
 nums2 = [[1,4],[3,2],[4,1]]
 result = []
+mem = []
 p = 0
 
 sum_val = 0
@@ -12,7 +12,18 @@ sum_val = 0
 while p < len(nums1):
      if nums1[p][0] == nums2[p][0]:
              sum_val = nums2[p][1] + nums1[p][1]
+             mem.append(nums1[p][0])
              result.append([nums1[p][0],sum_val])
      p +=1
 
-print(result)
+id_pointer = 0
+p = 0
+
+while p < len(nums1):
+        if nums1[p][0] not in mem:
+                result.append(nums1[p])
+        if nums2[p][0] not in mem:
+                result.append(nums2[p])
+        p +=1
+
+print(sorted(result))
