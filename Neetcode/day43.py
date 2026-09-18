e643f786308523bf3db0c828b5f21d034d1f347f
buildx2006 <mabarathkumar@gmail.com>
2026-09-18 08:33:48 +0530

    medium sum solved

--- Neetcode/day43.py
+++ Neetcode/day43.py
@@ -1,25 +1,45 @@
 #### hey hi this is barath kumar --today i am working on the 
 #### 2109. Adding Spaces to a String
 
-s = "LeetcodeHelpsMeLearn"
-spaces = [8,13,15]
+# s = "LeetcodeHelpsMeLearn"
+# spaces = [8,13,15]
 
-space = sorted(spaces)
-s_t = list(s)
+# space = sorted(spaces)
+# s_t = list(s)
+
+# pointer = 0
+# reader = 0
+# result = ""
+# p = 0
+
+# while pointer < len(space):
+#      s_t.insert(space[pointer]+p, " ")
+#      p +=1
+#      pointer +=1
+
+# s = "".join(s_t)
+
+# print(s)
+
+
+# (above_my logic)
+
+
 
 pointer = 0
 reader = 0
-result = ""
+result = []
+s = "LeetcodeHelpsMeLearn"
+spaces = [8,13,15]
+
 
 while pointer < len(s):
-     if space[reader] == pointer and reader == 0:
-          s_t.insert(space[reader] + 1 ," ")
-          reader +=1 
-     elif reader == pointer and reader > 1:
-          s_t.insert(space[reader]+1," ")
-          reader +=1
+     if  reader < len(spaces) and pointer == spaces[reader]: 
+                    result.append(" ")
+                    reader +=1
+     result.append(s[pointer])
      pointer +=1
 
-s = "".join(s_t)
+s = "".join(result)
 
 print(s)
