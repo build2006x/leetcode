52bfcb56f4a3e1e826479477fe9658543e266c73
buildx2006 <mabarathkumar@gmail.com>
2026-09-22 20:43:21 +0530

    Partition Array According to Given Pivot algo written

---
+++ Neetcode/day45.py
@@ -0,0 +1,19 @@
+### hey hi today i am working on the 2161. Partition Array According to Given Pivot
+nums = [9,12,5,10,14,3,10]
+pivot = 10
+
+"""
+steps to sovle this question i have my perspective of solving this question
+
+find the index of the pivot index (first loop before the main loop)
+assign that index to a variable 
+
+vaild index of storing would be less than the index (pivot) (less value than the pivot--variable_value)
+vaild index of storing would be greatern than indxe (pivot) (greater than the value of the pivot__variable_value)
+
+
+logic gain 
+
+3loop --- one for and two while to arrange range pivot_index - 1 and pivot_index + 1
+
+"""
