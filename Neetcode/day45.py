c2de0a6bd9e59be52c96e415b48c4d6817c8a66c
buildx2006 <mabarathkumar@gmail.com>
2026-09-22 22:29:41 +0530

    tomorrow i need to workout a constrain

--- Neetcode/day45.py
+++ Neetcode/day45.py
@@ -1,6 +1,6 @@
 ### hey hi today i am working on the 2161. Partition Array According to Given Pivot
-nums = [9,12,5,10,14,3,10]
-pivot = 10
+
+
 
 """
 steps to sovle this question i have my perspective of solving this question
@@ -17,3 +17,41 @@ logic gain
 3loop --- one for and two while to arrange range pivot_index - 1 and pivot_index + 1
 
 """
+
+nums = [-3,4,3,2]
+s = sorted(nums)
+pivot = 2
+small = []
+big = []
+main_idx = 0
+l = 0 
+r = 1 
+
+
+for idx,val in enumerate(nums):
+    if val == pivot:
+         main_idx = idx
+         l = main_idx - 1
+         r = main_idx + 1
+         break
+
+for idx,i in enumerate(s):
+     if i < pivot and idx != main_idx:
+          small.append(i)
+     elif i >= pivot and idx != main_idx:
+         big.append(i)
+
+small = sorted(small)
+big = sorted(big)
+
+for i in small:
+    if l >= 0:
+        nums[l] = i
+        l -= 1
+
+for j in big:
+    if r < len(nums):
+         nums[r] = j
+         r +=1
+
+print(nums)
