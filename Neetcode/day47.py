3d81686dfbc0d966046f4c0adaeebc6044257d10
buildx2006 <mabarathkumar@gmail.com>
2026-09-24 19:32:30 +0530

    brute force approach learnt how to handle the duplicate index

--- Neetcode/day47.py
+++ Neetcode/day47.py
@@ -1,21 +1,38 @@
 ### hey hi today i am working on the 3 sum question leetcode
 
-nums =  [-2,0,0,2,2]
-result = []
-pointer = 0
-l = 0
-r = 1
+nums=[-1,0,1,2,-1,-4,-2,-3,3,0,4]
+# result = []
+# stack1 = 0
+# stack2 = 1
+# die = stack2 + 1
 
 
-while pointer < len(nums):
-        while r < len(nums):
-                if l != pointer and pointer != r and l != r and  nums[l] + nums[r] + nums[pointer] == 0 and sorted([nums[pointer],nums[l],nums[r]]) not in result:
-                              print(pointer,l,r)
-                              result.append(sorted([nums[pointer],nums[l],nums[r]]))
-                l +=1
-                r +=1
-        r = 1
-        l = 0
-        pointer +=1
+# while stack2 < len(nums):
+#         while die < len(nums):
+#                 if die != stack2  and die != stack1 and  nums[stack1]+nums[stack2]+nums[die] == 0 and sorted([nums[stack1],nums[stack2],nums[die]]) not in result:
+#                                         result.append(sorted([nums[stack1],nums[stack2],nums[die]]))
+#                                         die +=1
+#                 else:
+#                         die +=1
+#         stack1 +=1
+#         stack2 = stack1 + 1 
+#         die = 0 
+
+result = set()
+count = 0
+for i in range(0,len(nums)):
+    for j in range(i+1,len(nums)):
+         for k in range(j+1,len(nums)):
+               if nums[i]+nums[j]+nums[k] == 0:
+                    result.add(tuple(sorted([nums[i], nums[j], nums[k]])))
+                
 
 print(result)
+print(count)
+
+
+### the good thing i learnt so far about the far whenever i am facing the index duplicate problem i will think to go for the for loop 
+### becuase the poitner appraoch needs to thinking more of the reseting correctly the variabel values 
+
+### i will go for the for loop only if the constrain are the less where the for loop will be working fine well 
+
